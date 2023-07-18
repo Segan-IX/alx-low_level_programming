97243c809d01285ca7b053699c1b534116952a4d
Sedem <sedemmensah10@gmail.com>
2023-07-18 16:26:09 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * main - print _putchar
- * Program prints _putchar, followed by a new line
+ * print_alphabet- prints the alphabet, in lowercase
+ * followed by a new line
+ * Return: Always 0
  */
-
-int main(void)
+void print_alphabet(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
 }
